d1720fdacbcd85096268a38adc8b5440f8893646
ahmed eid <ahmedeid6842@gmail.com>
2020-04-08 23:25:27 +0200

    my first commit

--- src/decoratorPattern/Restaurant.java
+++ src/decoratorPattern/Restaurant.java
@@ -5,6 +5,7 @@ public class Restaurant {
 	public static void main(String[] args) {
 	      //Decorator  Task 
               //BY : Ahmed Mahmoud Eid 
+              //Section 1
 		Pizza pizza2 = new Tuna();
 		pizza2 = new Tomato(pizza2);
 		pizza2 = new Pepper(pizza2);
